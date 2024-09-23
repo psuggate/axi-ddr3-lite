a0dbd662c0188036655f05caa7dc0cfc33c5006c
Patrick Suggate <patrick.suggate@gmail.com>
2024-09-23 16:30:03 +1200

    Merging latest.

--- rtl/arch/gw2a_rpll.v
+++ rtl/arch/gw2a_rpll.v
@@ -1,41 +1,36 @@
 `timescale 1ns / 100ps
-module gw2a_rpll (
-    clkout,  // 120 MHz by default
-    clockd,
-    lock,
-    clkin
+module gw2a_rpll #(
+    parameter FCLKIN = "27",
+    parameter DYN_IDIV_SEL = "false",
+    parameter IDIV_SEL = 8,
+    parameter DYN_FBDIV_SEL = "false",
+    parameter FBDIV_SEL = 39,
+    parameter DYN_ODIV_SEL = "false",
+    parameter ODIV_SEL = 8,
+    parameter PSDA_SEL = "0000",
+    parameter DYN_DA_EN = "false",
+    parameter DUTYDA_SEL = "1000",
+    parameter CLKOUT_FT_DIR = 1'b1,
+    parameter CLKOUTP_FT_DIR = 1'b1,
+    parameter CLKOUT_DLY_STEP = 0,
+    parameter CLKOUTP_DLY_STEP = 0,
+    parameter CLKFB_SEL = "internal",
+    parameter CLKOUT_BYPASS = "false",
+    parameter CLKOUTP_BYPASS = "false",
+    parameter CLKOUTD_BYPASS = "false",
+    parameter DYN_SDIV_SEL = 2,
+    parameter CLKOUTD_SRC = "CLKOUT",
+    parameter CLKOUTD3_SRC = "CLKOUT",
+    parameter DEVICE = "GW2A-18C"
+) (
+    output clkout,  // 120 MHz by default
+    output clockd,
+    output clockp,
+    output lock,
+    input  clkin,
+    input  reset
 );
 
-  parameter FCLKIN = "27";
-  parameter DYN_IDIV_SEL = "false";
-  parameter IDIV_SEL = 8;
-  parameter DYN_FBDIV_SEL = "false";
-  parameter FBDIV_SEL = 39;
-  parameter DYN_ODIV_SEL = "false";
-  parameter ODIV_SEL = 8;
-  parameter PSDA_SEL = "0000";
-  parameter DYN_DA_EN = "true";
-  parameter DUTYDA_SEL = "1000";
-  parameter CLKOUT_FT_DIR = 1'b1;
-  parameter CLKOUTP_FT_DIR = 1'b1;
-  parameter CLKOUT_DLY_STEP = 0;
-  parameter CLKOUTP_DLY_STEP = 0;
-  parameter CLKFB_SEL = "internal";
-  parameter CLKOUT_BYPASS = "false";
-  parameter CLKOUTP_BYPASS = "false";
-  parameter CLKOUTD_BYPASS = "false";
-  parameter DYN_SDIV_SEL = 2;
-  parameter CLKOUTD_SRC = "CLKOUT";
-  parameter CLKOUTD3_SRC = "CLKOUT";
-  parameter DEVICE = "GW2A-18C";
-
-  output clkout;
-  output clockd;
-  output lock;
-  input clkin;
-
-  wire clkoutp_o;
-  wire clkoutd_o;
   wire clkoutd3_o;
   wire gw_gnd;
 
@@ -67,10 +62,10 @@ module gw2a_rpll (
   ) rpll_inst (
       .CLKOUT(clkout),
       .LOCK(lock),
-      .CLKOUTP(clkoutp_o),
+      .CLKOUTP(clockp),
       .CLKOUTD(clockd),
       .CLKOUTD3(clkoutd3_o),
-      .RESET(gw_gnd),
+      .RESET(reset),
       .RESET_P(gw_gnd),
       .CLKIN(clkin),
       .CLKFB(gw_gnd),
